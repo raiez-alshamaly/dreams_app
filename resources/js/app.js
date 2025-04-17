c0ac59a8d62f07ba4da2d08fb8a954cfe66fd3db
mekadalibrahem <mekadalibrahem123@gmail.com>
2025-04-17 16:38:01 +0300

    Update package dependencies to include vanilla-calendar-pro and adjust import statements in app.js for better organization.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
-import './bootstrap';
-// import './bootstrap.bundle.min.js';
-import './jquery-3.6.0.min.js';
 
-import './scripts.js';
+// import './bootstrap';
+// import './bootstrap.bundle.min.js';
+// import './jquery-3.6.0.min.js';
+
 import 'preline'
+import './scripts.js';
