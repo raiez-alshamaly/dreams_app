7b741b0885fc0c79ba42b529e16dd5d74519b079
mekadalibrahem <mekadalibrahem123@gmail.com>
2025-04-18 12:38:42 +0300

    add SofitDealtes

--- app/Models/Dream.php
+++ app/Models/Dream.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Dream extends Model
 {
     /** @use HasFactory<\Database\Factories\DreamFactory> */
     use HasFactory;
-
+    use SoftDeletes;
     protected $fillable = [
         'id',
         'full_name',
@@ -20,4 +21,7 @@ class Dream extends Model
         'deleted_at',
     ];
     public $timestamps = true;
+
+
+   
 }
